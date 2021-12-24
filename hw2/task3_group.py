5fff0809e2bd370f1f3cb061a1cc032cfed25d5e
amuriks7 <amuriks7@gmail.com>
2021-12-24 14:19:35 +0200

    upd age

--- hw2/task3_group.py
+++ hw2/task3_group.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
 
     for index in range(1, 11):
         group.add(
-            Student(f'Petrov{index}', 20 + index, 'male', 4)
+            Student(f'Petrov{index}', 19 + index, 'male', 4)
         )
 
     print(group)
